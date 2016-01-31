792a40bdb6f0e1f5ea8c2da810ef68f0dfab1b5c
Daniel Segal <dsegal92@gmail.com>
2016-01-31 15:11:57 -0500

    Upsert most_recently_seen and track first seen

--- db.go
+++ db.go
@@ -14,6 +14,7 @@ var DB_PASS = os.Getenv("INSTAGRAM_DB_PASS")
 
 func insertFollows(follows []string) {
 	connection_url := fmt.Sprintf("root:%v@tcp(%v:3306)/instagram_statistics", DB_PASS, DB_URL)
+	updateTime := time.Now()
 
 	db, err := sql.Open("mysql", connection_url)
 	if err != nil {
@@ -26,13 +27,16 @@ func insertFollows(follows []string) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	stmtIns, err := db.Prepare("INSERT INTO Follows VALUES (null, ?, ?)")
+	stmtIns, err := db.Prepare(`INSERT INTO Follows (id, username, most_recently_seen, first_seen)
+															VALUES (null, ?, ?, ?)
+															on duplicate key update
+																most_recently_seen = values(most_recently_seen)`)
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer stmtIns.Close()
 
 	for i := 0; i < len(follows); i++ {
-		_, err = stmtIns.Exec(follows[i], time.Now())
+		_, err = stmtIns.Exec(follows[i], updateTime, updateTime)
 	}
 }
